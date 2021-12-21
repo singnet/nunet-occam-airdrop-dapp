a222da4f8a8ac7be45834363d3953e772775ec9d
Susmithashanmugam <87366584+Susmithageek1@users.noreply.github.com>
2021-12-22 01:56:36 +0530

    Added changes in the portal

--- pages/index.tsx
+++ pages/index.tsx
@@ -252,13 +252,13 @@ const Home: NextPage = () => {
       />
       <Airdroprules
         title="Airdrop Rules"
-        steps={airdropRules}
+        steps={RulesSampleData}
         blogLink="https://medium.com/occam-finance/nunet-backed-by-singularitynet-to-hold-ido-on-occamrazer-7e9eab947add"
         ref={rulesRef}
       />
 
       <AirdropSchedules ref={scheduleRef} schedules={schedules} />
-      <Ecosystem blogLink="https://medium.com/occam-finance/nunet-backed-by-singularitynet-to-hold-ido-on-occamrazer-7e9eab947add" />
+      <Ecosystem blogLink="https://singularitynet.io/" />
 
       
     </CommonLayout>
@@ -289,4 +289,31 @@ const HowItWorksSampleData = [
       "You can choose to claim both allocations at once after the second allocation unlocks on January 26th, to save gas fees.",
   },
   
+];
+const RulesSampleData = [
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing an",
+    description:
+      "typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised",
+  },
+  {
+    title: "It is a long established fact that a",
+    description:
+      " is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions ",
+  },
+  {
+    title: "Contrary to popular belief, Lorem Ipsum is not si",
+    description:
+      "andom text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem I",
+  },
+  {
+    title: "Where can I get some?",
+    description:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generat",
+  },
+  {
+    title: "atise on the theory of ethics, very popu",
+    description:
+      "ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, c",
+  },
 ];
