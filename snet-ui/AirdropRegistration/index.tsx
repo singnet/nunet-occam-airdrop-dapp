535bbffaf80c8fc3b33e4c72656666de195ae612
anandrgitnirman <NIrman$123>
2022-01-26 16:35:02 +0530

    Disable the stake as pool is full

--- snet-ui/AirdropRegistration/index.tsx
+++ snet-ui/AirdropRegistration/index.tsx
@@ -373,7 +373,7 @@ export default function AirdropRegistration({
                   }}
                   onClick={toggleStakeModal}
                   loading={claimLoader}
-                  disabled={!stakeInfo.is_stakable}
+                  disabled={true}
                 >
                   Stake
                 </LoadingButton>
