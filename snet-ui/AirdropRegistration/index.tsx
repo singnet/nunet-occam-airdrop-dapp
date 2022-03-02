811a148473f1db606b6b0ae26f11cf83adece3ee
anandrgitnirman <NIrman$123>
2022-03-02 16:23:50 +0530

    REfer to the name of the token from the backend reponse returned

--- snet-ui/AirdropRegistration/index.tsx
+++ snet-ui/AirdropRegistration/index.tsx
@@ -31,7 +31,7 @@ type HistoryEvent = {
 type StakeInfo = {
   claimableTokensToWallet: string;
   isStakable: boolean;
-  stakableTokenName: string;
+  tokenName: string;
   stakableTokens: string;
   isLoading: boolean;
 };
@@ -215,7 +215,7 @@ export default function AirdropRegistration({
             </Grid>
             <Grid item xs={4}>
               <Typography variant="h6">
-                {`${Number(stakeInfo.stakable_tokens) / 1000000} ${stakeInfo.stakable_token_name}`}
+                {`${Number(stakeInfo.stakable_tokens) / 1000000} ${stakeInfo.token_name}`}
 
               </Typography>
             </Grid>
